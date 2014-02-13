b46a3dd852a47da11015327a8f232db6348c04ba
Willian <willian@pythonheads.nl>
2014-02-13 13:25:15 +0100

    Renamed product to variant

--- app/services/event_service.rb
+++ app/services/event_service.rb
@@ -1,11 +1,21 @@
 class EventService < BaseService
-  def initialize(product, user, type)
-    @product = product
+  def initialize(variant, user, type)
+    @variant = variant
     @user = user
     @type = type
   end
 
   def push_to_layer
+    event_data = { user_id: user_layer_id,
+                   item_id: variant_layer_id }
+    client.post 'events', event_data
+  end
 
+  def user_layer_id
+    UserService.new(@user).layer_id
+  end
+
+  def variant_layer_id
+    VariantService.new(@product).layer_id
   end
 end
